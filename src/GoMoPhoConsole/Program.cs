3d80cfa3ddb0534dc98ae8a1f05415b82488a6c1
Clive Gout <35511400+cliveontoast@users.noreply.github.com>
2018-11-25 20:54:09 +0800

    Trialing gif output

--- src/GoMoPhoConsole/Program.cs
+++ src/GoMoPhoConsole/Program.cs
@@ -1,14 +1,25 @@
 ﻿using GoMoPho;
 using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xabe.FFmpeg;
 
 namespace GoMoPhoConsole
 {
     class Program
     {
+        static ConcurrentQueue<FileInfo> filesToConvert;
+
         static void Main(string[] args)
         {
+            
+            filesToConvert = new ConcurrentQueue<FileInfo>();
+
             string searchPattern = System.Configuration.ConfigurationManager.AppSettings["FilePattern"];
             string video = ".mp4";
             var hexSearches = System.Configuration.ConfigurationManager.AppSettings.AllKeys.Where(a => a.StartsWith("Mp4Header")).ToList();
@@ -71,6 +82,7 @@ namespace GoMoPhoConsole
                 }
             }
             Console.WriteLine($"Finished. Processed {count} files, found {successCount} videos for {imageFiles.Length} images files.");
+            Run().GetAwaiter().GetResult();
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
@@ -145,6 +157,66 @@ namespace GoMoPhoConsole
                 mp4Stream.Seek(0, System.IO.SeekOrigin.Begin);
                 mp4Stream.Write(fileBytes, indexOfMp4, fileBytes.Length - indexOfMp4);
             }
+            filesToConvert.Enqueue(new FileInfo(mp4File));
+        }
+
+
+
+        private static async Task Run()
+        {
+            await Console.Out.WriteLineAsync($"Find {filesToConvert.Count} files to convert.");
+            //Run conversion
+            await RunConversion(filesToConvert);
+        }
+
+        private static IEnumerable<FileInfo> GetFilesToConvert(string directoryPath)
+        {
+            //Return all files excluding mp4 because I want convert it to mp4
+            return new DirectoryInfo(directoryPath).GetFiles().Where(x => x.Extension != ".mp4").ToList();
+        }
+
+        private static async Task RunConversion(ConcurrentQueue<FileInfo> filesToConvert)
+        {
+            string outputFileName = null;
+            while (filesToConvert.TryDequeue(out FileInfo fileToConvert))
+            {
+                try
+                {
+                    outputFileName = await OutputGif(fileToConvert);
+                }
+                catch (Exception e)
+                {
+                    if (FFmpeg.ExecutablesPath == null)
+                    {
+                        FFmpeg.ExecutablesPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "FFmpeg");
+                        //Get latest version of FFmpeg. It's great idea if you don't know if you had installed FFmpeg.
+                        await Console.Out.WriteLineAsync($"Getting FFMpeg, storing {FFmpeg.ExecutablesPath}");
+                        await FFmpeg.GetLatestVersion();
+                        filesToConvert.Enqueue(fileToConvert);
+                    }
+                    else
+                    {
+                        await Console.Out.WriteLineAsync($"Failed file {fileToConvert.Name} to {outputFileName}");
+                        while (e != null)
+                        {
+                            await Console.Out.WriteLineAsync(e.Message);
+                            await Console.Out.WriteLineAsync(e.StackTrace);
+                            e = e.InnerException;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static async Task<string> OutputGif(FileInfo fileToConvert)
+        {
+            //Save file to the same location with changed extension
+            string outputFileName = Path.ChangeExtension(fileToConvert.FullName, ".gif");
+            File.Delete(outputFileName);
+            await Console.Out.WriteLineAsync($"Writing to {outputFileName}");
+            await Conversion.ToGif(fileToConvert.FullName, outputFileName, 0).Start();
+            await Console.Out.WriteLineAsync($"Finished converion file [{fileToConvert.Name}] to .gif");
+            return outputFileName;
         }
     }
 }
